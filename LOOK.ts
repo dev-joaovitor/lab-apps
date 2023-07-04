ca76fc52c00781cefeaf6b55fa207626c6e58f63
joao vitor <joaotorvitor1@gmail.com>
2023-07-04 14:57:07 -0300

    adjustments

---
+++ LOOK.ts
@@ -0,0 +1,100 @@
+`
+/src/entity/user.entity.ts
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+}
+
+//////////
+
+
+/src/app-data-source.ts
+import { DataSource } from "typeorm"
+
+export const myDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "postgres",
+    database: "labambev",
+    entities: ["src/entity/*.ts"],
+    logging: true,
+    synchronize: true,
+})
+
+
+///////
+
+/src/app.ts
+import * as express from "express"
+import { Request, Response } from "express"
+import { myDataSource } from "./app-data-source"
+import { User } from "./entity/user.entity"
+
+// establish database connection
+myDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
+
+// create and setup express app
+const app = express()
+app.use(express.json())
+
+// register routes
+app.get("/users", async function (req: Request, res: Response) {
+    const users = await myDataSource.getRepository(User).find()
+    res.json(users)
+})
+
+app.get("/users/:id", async function (req: Request, res: Response) {
+    const results = await myDataSource.getRepository(User).findOneBy({
+        id: parseInt(req.params.id),
+    })
+    return res.send(results)
+})
+
+app.post("/users", async function (req: Request, res: Response) {
+    console.log(req.body);
+    const user = myDataSource.getRepository(User).create(req.body)
+    const results = await myDataSource.getRepository(User).save(user).catch(e => {
+        res.status(400).send({ "error": "=(" });
+        return;
+    })
+
+    return res.send(results);
+})
+
+app.put("/users/:id", async function (req: Request, res: Response) {
+    const user = await myDataSource.getRepository(User).findOneBy({
+        id: parseInt(req.params.id),
+    })
+    myDataSource.getRepository(User).merge(user, req.body)
+    const results = await myDataSource.getRepository(User).save(user)
+    return res.send(results)
+})
+
+app.delete("/users/:id", async function (req: Request, res: Response) {
+    const results = await myDataSource.getRepository(User).delete(req.params.id)
+    return res.send(results)
+})
+
+// start express server
+app.listen(3000)
+
+
+`
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import express from 'express';
+import express from "express";
 
 const app = express();
 
