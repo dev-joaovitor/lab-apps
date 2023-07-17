e7761fad59eb3c01dbc965b6fef6b239cd5593d4
joao vitor <joaotorvitor1@gmail.com>
2023-07-17 14:56:30 -0300

    pdf skalar changes

--- src/controller/SkalarControllers.ts
+++ src/controller/SkalarControllers.ts
@@ -12,23 +12,39 @@ export async function extractSkalarData(req: Request, res: Response) {
 
     const resultData = {};
 
-    pdf(file).then((res: any) => {
-        // raw parsed text from pdf
-        const text = res.text;
-    
-        // only the tests filtered
-        const allTests = text.split("\n").slice(42);
-    
-        // only the "U" sample type tests
-        const u_Tests = allTests.map((str: string) => str.slice(str.indexOf("U"))).filter(value => value.length > 2);
-    
-        // array of strings transformed in array
-        const newArr = u_Tests.map((str: string) => str.toUpperCase().slice(1).split(""));
+    let sample = ["fanod-32-32", "firocs-32-23", "FANBRA-BRA2-329"];
 
-        // final test object fill with => key = sample id && value = fan-results
-        newArr.map((item: string[]) => resultData[item.slice(0,4).join("")] = parseFloat(item.slice(4).join("").replace(",",".")));
+    console.log(sample.map(e => e.toUpperCase()).filter(e => e.includes("FAN")));
+
+
+    pdf(file).then((res: any) => {
+        // // raw parsed text from pdf
+        // const text = res.text;
+        // console.log(text)
+        // console.log("\n\n")
+    
+        // // only the tests filtered
+        // const allTests = text.split("\n").slice(42);
+        // console.log(allTests)
+        // console.log("\n\n")
+        
+        // // only the "U" sample type tests
+        // const u_Tests = allTests.map((str: string) => str.slice(str.indexOf("U"))).filter(value => value.length > 2);
+        // console.log(u_Tests)
+        // console.log("\n\n")
+
+        // // array of strings transformed in array
+        // const newArr = u_Tests.map((str: string) => str.toUpperCase().slice(1).split(""));
+        // console.log(newArr)
+        // console.log("\n\n")
+
+        // // final test object fill with => key = sample id && value = fan-results
+        // newArr.map((item: string[]) => resultData[item.slice(0,4).join("")] = parseFloat(item.slice(4).join("").replace(",",".")));
+        // console.log(resultData)
+        // console.log("\n\n")
     
     }).finally(() => {
-        return res.status(200).json({ status: "success", message: JSON.stringify(resultData) });
+        // return res.status(200).json({ status: "success", message: JSON.stringify(resultData) });
+        return res.status(200).json({ status: "success", message: JSON.stringify({ ":)": ":D"}) });
     })
 };
