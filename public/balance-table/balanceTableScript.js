3ff3daa5cbc75afa0b9bb45b786e44cce0631155
joao vitor <joaotorvitor1@gmail.com>
2023-07-21 14:45:02 -0300

    changes

--- public/balance-table/balanceTableScript.js
+++ public/balance-table/balanceTableScript.js
@@ -6,7 +6,8 @@ const weightTable = document.querySelector("#weight-table"),
       tableResult = document.querySelector("p#weight-table-result span"),
       calcCriteriaBtn = document.querySelector("#calc-criteria"),
       criteriaIndividualVolume = document.querySelector("#criteria-individual-volume"),
-      criteriaAverageVolume = document.querySelector("#criteria-average-volume");
+      criteriaAverageVolume = document.querySelector("#criteria-average-volume"),
+      resetBtn = document.querySelector("#reset-btn");
 
 weightTable.addEventListener("click", (e) => {
     if (e.target.nodeName === "TD" && !e.target.className) {
@@ -72,4 +73,12 @@ calcCriteriaBtn.addEventListener("click", async () => {
     criteriaAverageVolume.classList.add(`criteria-${CIV.toLowerCase()}`);
 
     if (CIV === "OK" && CAV === "OK") sendWeightsBtn.disabled = false;
+})
+
+resetBtn.addEventListener("click", (e) => {
+    if (!confirm('Seus dados serão apagados, deseja continuar?')) return;
+    
+    document.cookie = "form=; Max-age=-1; Path=/";
+    document.cookie = "weights=; Max-age=-1";
+    location.reload();
 })
